9da0bbeb8031ed38ac81cd8ee2d1d2b3f53e115c
He3amtesla <128234173+He3amtesla@users.noreply.github.com>
2025-01-12 12:59:08 +0330

    Update game2.py
    Improve code readability by refactoring
    - Add documentation to scripts, classes, and methods
    - Update and refine type hints for better clarity

--- src/game2.py
+++ src/game2.py
@@ -1,11 +1,21 @@
+"""
+   author: MohammadHesam Pourakbar
+   date: 2025.1.12
+   summery: The game RockPaperScissors
+"""
+
+
 import random
-from typing import List
+from typing import List, Tuple
 
 class RockPaperScissors:
+    """This is main class RockPaperScissors"""
+    
     def __init__(self):
-        self.list_choises: list[str] = ["rock","paper","scissors"]
+        self.list_choises: List[str] = ["rock","paper","scissors"]
     
     def get_user_choice(self) -> str:
+        """user gets random --> ["rock","paper","scissors"]"""  
         player_choice: str = input(f"please choice this one to run to game: {self.list_choises}: ")
         
         if player_choice.lower() in self.list_choises:
@@ -16,15 +26,22 @@ class RockPaperScissors:
         return self.get_user_choice()
         
     def get_computer_choice(self) -> str:
+        """Computer gets random --> ["rock","paper","scissors"]"""  
         computer_choice: str = random.choice(self.list_choises)
         print(f"Computer Choice: {computer_choice}")
         return computer_choice
         
     def choice_winner(self, user: str, computer: str) -> str:
+        """this method: who is the winner (computer or user)
+        
+        :param user: The user choice, Must be one of ["rock","paper","scissors"]
+        :param computer: The computer choice, Must be one of ["rock","paper","scissors"]
+        :return: who is won!
+        """
         if user == computer:
             return "It's Tie"
         
-        user_win: list[str] = [("paper", "rock"), ("rock", "scissors"), ("scissors", "paper")]
+        user_win: List[Tuple[str, str]] = [("paper", "rock"), ("rock", "scissors"), ("scissors", "paper")]
         
         for i in user_win:
             if (user == i[0]) and (computer== i[1]):
@@ -33,6 +50,7 @@ class RockPaperScissors:
         return "You Lose"
     
     def play(self):
+        """one of the main methode, run this project"""
         user_choice: str = self.get_user_choice()
         computer_choice: str = self.get_computer_choice()
         #print(f"Computer Choise: {self.get_computer_choice()}")
